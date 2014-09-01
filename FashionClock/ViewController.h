595de61fe0a081f9e14388377cdc19601038e5f9
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-09-01 17:06:10 -0300

    Initial Commit

---
+++ FashionClock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FashionClock
+//
+//  Created by Marcelo Sampaio on 9/1/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
